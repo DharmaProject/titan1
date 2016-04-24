b52c7f7534ce3ffa3db8b4d8f8f39437bc4c0620
Viri Cruz <viridiana.cruz.olivera@gmail.com>
2016-04-24 02:51:58 -0700

    UI con vuforia

---
+++ vuforiaUI/Assets/ProgressCircle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using ProgressBar;
+
+
+public class ProgressCircle : MonoBehaviour
+{
+
+    public ProgressRadialBehaviour prb;
+
+    public float valor;
+    // Use this for initialization
+    void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        Debug.Log(valor);
+        
+	}
+
+    void LateUpdate() {
+        prb.Value = valor;
+    }
+
+}
